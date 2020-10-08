c88663f053747c374a9366bf91d12bfedb6e2881
Tiago Morais Rosa Luiz <tiagomrluiz@gmail.com>
2020-10-08 19:29:16 -0300

    commit atualização dart

---
+++ fundamentos/notacao_ponto.dart
@@ -0,0 +1,10 @@
+main() {
+  double nota = 6.99.roundToDouble();
+  print(nota);
+
+  print("texto".toUpperCase());
+
+  String s1 = "tiago luiz";
+  String s2 = s1.substring(0, 8);
+  print(s2);
+}
