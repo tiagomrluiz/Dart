cf814b003162aa6f865e58cf873931c47f75c7df
Tiago Morais Rosa Luiz <tiagomrluiz@gmail.com>
2020-10-09 18:38:38 -0300

    commit dart 09-10-20 fundamentos

---
+++ fundamentos/generics.dart
@@ -0,0 +1,14 @@
+main() {
+  List<String> frutas = ['banana', 'maça', 'laranja'];
+  frutas.add('melão');
+
+  print(frutas);
+
+  Maq<String, double> salarios = {
+    'gerente': 19345.78,
+    'vendedor': 16345.80,
+    'estagiário': 600.00,
+  };
+
+  print(salarios);
+}
